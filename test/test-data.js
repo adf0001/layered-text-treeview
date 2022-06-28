48763ad0d83e519d50dafd789b32a6fb0f3cc562
fwg <adf0001@163.com>
2022-06-28 17:18:27 +0800

    change: use treeview-model; split to lib;

--- test/test-data.js
+++ test/test-data.js
@@ -1,130 +1,161 @@
 
 //global variable, for html page, refer tpsvr @ npm.
 layered_text_treeview = require("../layered-text-treeview.js");
-ui_model_treeview = require("ui-model-treeview");
 layered_text = require("layered-text");
+
+base_test_data = require("treeview-model/test/test-data.js");
 
 module.exports = {
 
-	"layered_text_treeview": function (done) {
+	"layered_text_treeview": function (done, treeviewModel) {
 		if (typeof window === "undefined") throw "disable for nodejs";
 
-		//dom
-		document.getElementById('divResult3').innerHTML =
-			"<div id='name-click-msg' style='border:1px solid lightgrey;'>&nbsp;</div>" +
-			"<div>" +
-			"<span class='ht cmd' id='sp-cmd-add'>+add</span>/" +
-			"<span class='ht cmd' id='sp-cmd-add-lt' title='layered-text'>lt</span> " +
-			"<span class='ht cmd' id='sp-cmd-insert'>+insert</span>/" +
-			"<span class='ht cmd' id='sp-cmd-insert-lt' title='layered-text'>lt</span> " +
-			"<span class='ht cmd' id='sp-cmd-insert-next'>+insert-next</span>/" +
-			"<span class='ht cmd' id='sp-cmd-insert-next-lt' title='layered-text'>lt</span> &nbsp; " +
-			"<span class='ht cmd' id='sp-cmd-remove'>-remove</span> " +
-			"<span class='ht cmd' id='sp-cmd-remove-prop'>-remove prop</span> " +
-			"<span class='ht cmd' id='sp-cmd-remove-children'>-remove children</span> &nbsp; " +
-			"<span class='ht cmd' id='sp-cmd-update'>=update</span> &nbsp; " +
-			"<label><input type='checkbox' id='chk-update-select' checked/>update-select</label> &nbsp; " +
-			"depth<select id='selDepth'>" +
-			"	<option value='1' selected>1/other</option>" +
-			"	<option value='2'>2</option>" +
-			"	<option value='3'>3</option>" +
-			"	<option value='-1'>-1/all</option>" +
-			"</select> &nbsp; " +
-			"showProperty<select id='selShowProperty'>" +
-			"	<option value='ellipsis'>ellipsis</option>" +
-			"	<option value='first'>first</option>" +
-			"	<option value='true'>true</option>" +
-			"	<option value='' selected>false</option>" +
-			"</select>" +
-			"</div>" +
-			"<div id='lt-treeview'></div>";
+		if (treeviewModel) layered_text_treeview = layered_text_treeview;
 
-		var el = document.getElementById('lt-treeview');
+		base_test_data["level-3"](
+			function (err, data) {
+				if (err) { done(err); return; }
 
-		var data = ["aaa", "bbb",
-			[
-				"ccc If the input element exists in the list between the specified indices, the index () method returns the index of the element where it occurs first. We can observe this in the following example.",
-				{ b: 2 }, ["eee"], "ddd"
-			]
-		];
+				var container = layered_text_treeview.getContainer("nd1");
 
-		//.class(el, layeredText)
-		var tv = new layered_text_treeview.class(el);
+				var elTool = document.getElementById('div-tool');
+				elTool.insertAdjacentHTML("beforeend",
+					'<div style="border-bottom:1px solid gray;padding-bottom:0.3em;">' +
+					'level-4/layered-text-treeview: ' +
 
-		//tv.showProperty = "ellipsis";
-		//tv.showProperty = "first";
-		//tv.showProperty = true;
-		tv.showProperty = false;
+					"<span id='name-click-msg'>&nbsp;</span>" +
+					"<div>" +
+					"<span class='-ht-cmd' id='sp-cmd-add'>+add</span>/" +
+					"<span class='-ht-cmd' id='sp-cmd-add-lt' title='layered-text'>lt</span> " +
+					"<span class='-ht-cmd' id='sp-cmd-insert'>+insert</span>/" +
+					"<span class='-ht-cmd' id='sp-cmd-insert-lt' title='layered-text'>lt</span> " +
+					"<span class='-ht-cmd' id='sp-cmd-insert-next'>+insert-next</span>/" +
+					"<span class='-ht-cmd' id='sp-cmd-insert-next-lt' title='layered-text'>lt</span> &nbsp; " +
+					"<span class='-ht-cmd' id='sp-cmd-remove'>-remove</span>/" +
+					"<span class='-ht-cmd' id='sp-cmd-remove-children' title='remove children'>-children</span>/" +
+					"<span class='-ht-cmd' id='sp-cmd-remove-prop' title='remove property'>-property</span> " +
+					"<span class='-ht-cmd' id='sp-cmd-update'>=update</span> &nbsp; " +
+					"depth<select id='selDepth'>" +
+					"	<option value='1' selected>1/other</option>" +
+					"	<option value='2'>2</option>" +
+					"	<option value='3'>3</option>" +
+					"	<option value='-1'>-1/all</option>" +
+					"</select> &nbsp; " +
+					"showProperty<select id='selShowProperty'>" +
+					"	<option value='ellipsis'>ellipsis</option>" +
+					"	<option value='first'>first</option>" +
+					"	<option value='true'>true</option>" +
+					"	<option value='' selected>false</option>" +
+					"</select>" +
+					"</div>" +
+					"</div>"
+				);
 
-		//.updateView(layeredText)
-		tv.updateView(layered_text.normalize(data, true));
+				var data = ["aaa", "bbb",
+					[
+						"ccc If the input element exists in the list between the specified indices, the index () method returns the index of the element where it occurs first. We can observe this in the following example.",
+						{ b: 2, c: 3 }, ["eee"], "ddd"
+					]
+				];
 
-		el.addEventListener("click", function (evt) {
-			var target = evt.target;
-			if (target && target.classList.contains("tree-name")) {
-				var s = target.textContent;
-				if ((s.length > 50)) s = s.slice(0, 50) + "...";
+				//.updateView(layeredText)
+				layered_text_treeview.initView(container, layered_text.normalize(data, true,
+					{ showProperty: document.getElementById('selShowProperty').value }));
 
-				//.getDataInfo(elNode, dataInfo)
-				var prop = tv.getDataProperty(target);
-				s += " prop=" + JSON.stringify(prop);
+				container.addEventListener("click", function (evt) {
+					var target = evt.target;
 
-				document.getElementById('name-click-msg').innerHTML = s;
-			}
-		})
+					var elSelOne = layered_text_treeview.getOneSelected(target);
+					if (elSelOne) {
+						var s = layered_text_treeview.nodeName(elSelOne).textContent;
+						if ((s.length > 50)) s = s.slice(0, 50) + "...";
 
-		document.getElementById('selShowProperty').onchange = function (evt) {
-			tv.showProperty = evt.target.value;
-		};
+						//.getDataInfo(elNode, dataInfo)
+						var prop = layered_text_treeview.getDataProperty(elSelOne);
+						s += " prop=" + JSON.stringify(prop);
 
-		document.getElementById('sp-cmd-add').onclick = function () {
-			//.add(elNode, text, property, options)
-			tv.add(tv.getSelected() || el, "" + (new Date()), { tm: (new Date()).getTime() },
-				{ updateSelect: _ele('chk-update-select').checked });
-		};
-		document.getElementById('sp-cmd-add-lt').onclick = function () {
-			tv.add(tv.getSelected() || el, layered_text.normalize(data, true), null,
-				{ updateSelect: _ele('chk-update-select').checked, depth: _ele('selDepth').value });
-		};
-		document.getElementById('sp-cmd-insert').onclick = function () {
-			tv.add(tv.getSelected() || el, "" + new Date(), { tm: (new Date()).getTime() },
-				{ insert: true, updateSelect: _ele('chk-update-select').checked });
-		};
-		document.getElementById('sp-cmd-insert-lt').onclick = function () {
-			tv.add(tv.getSelected() || el, layered_text.normalize(data, true), null,
-				{ insert: true, updateSelect: _ele('chk-update-select').checked, depth: _ele('selDepth').value });
-		};
-		document.getElementById('sp-cmd-insert-next').onclick = function () {
-			//.insertNext(elNode, text, property, options)
-			if (tv.getSelected()) tv.insertNext(null, "" + new Date(), { tm: (new Date()).getTime() },
-				{ updateSelect: _ele('chk-update-select').checked });
-			else tv.add(el, "" + (new Date()), { tm: (new Date()).getTime() },
-				{ updateSelect: _ele('chk-update-select').checked });
-		};
-		document.getElementById('sp-cmd-insert-next-lt').onclick = function () {
-			//.insertNext(elNode, text, property, options)
-			if (tv.getSelected()) tv.insertNext(null, layered_text.normalize(data, true), null,
-				{ updateSelect: _ele('chk-update-select').checked, depth: _ele('selDepth').value });
-			else tv.add(el, layered_text.normalize(data, true), null,
-				{ updateSelect: _ele('chk-update-select').checked, depth: _ele('selDepth').value });
-		};
-		document.getElementById('sp-cmd-remove').onclick = function () {
-			//.remove(elNode, options)
-			tv.remove(null, { updateSelect: _ele('chk-update-select').checked });	//remove the selected
-		};
-		document.getElementById('sp-cmd-remove-prop').onclick = function () {
-			tv.removeProperty(null, { updateSelect: _ele('chk-update-select').checked });
-		};
-		document.getElementById('sp-cmd-remove-children').onclick = function () {
-			tv.removeAllChildren(null, { updateSelect: _ele('chk-update-select').checked });
-		};
-		document.getElementById('sp-cmd-update').onclick = function () {
-			//.update(elNode, text, property, options)
-			tv.update(null, "" + (new Date()), { tm: (new Date()).getTime() },		//update the selected
-				{ updateSelect: _ele('chk-update-select').checked });
-		};
+						document.getElementById('name-click-msg').innerHTML = s;
+					}
+				})
 
-		return "ui-test";
+				document.getElementById('selShowProperty').onchange = function (evt) {
+					var options = layered_text_treeview.getOptions(container);
+					options.showProperty = evt.target.value;
+				};
+
+				document.getElementById('sp-cmd-add').onclick =
+					document.getElementById('sp-cmd-add-lt').onclick =
+					document.getElementById('sp-cmd-insert').onclick =
+					document.getElementById('sp-cmd-insert-lt').onclick =
+					document.getElementById('sp-cmd-insert-next').onclick =
+					document.getElementById('sp-cmd-insert-next-lt').onclick =
+					document.getElementById('sp-cmd-remove').onclick =
+					document.getElementById('sp-cmd-remove-children').onclick =
+					document.getElementById('sp-cmd-remove-prop').onclick =
+					document.getElementById('sp-cmd-update').onclick =
+					function (evt) {
+						var cmdId = evt?.target?.id;
+
+						var elSel = layered_text_treeview.getSelected(container, true);
+						var elSelOne = (elSel instanceof Array) ? elSel[elSel.length - 1] : elSel;
+
+						var newName = (new Date()).toLocaleString();
+						var newProp = { tm: (new Date()).getTime() };
+						var elNew;
+
+						var depthValue = parseInt(_ele('selDepth').value);
+
+						if (cmdId === "sp-cmd-add") {
+							elNew = layered_text_treeview.add(elSelOne || container,
+								{ text: newName, property: newProp }, null, !elSelOne);
+						}
+						else if (cmdId === "sp-cmd-add-lt") {
+							elNew = layered_text_treeview.add(elSelOne || container,
+								layered_text.normalize(data, true), { depth: depthValue }, !elSelOne);
+						}
+						else if (cmdId === "sp-cmd-insert") {
+							if (elSelOne) elNew = layered_text_treeview.insert(elSelOne,
+								{ text: newName, property: newProp });
+						}
+						else if (cmdId === "sp-cmd-insert-lt") {
+							if (elSelOne) elNew = layered_text_treeview.insert(elSelOne,
+								layered_text.normalize(data, true), { depth: depthValue });
+						}
+						else if (cmdId === "sp-cmd-insert-next") {
+							if (elSelOne) elNew = layered_text_treeview.insertNext(elSelOne,
+								{ text: newName, property: newProp });
+						}
+						else if (cmdId === "sp-cmd-insert-next-lt") {
+							if (elSelOne) elNew = layered_text_treeview.insertNext(elSelOne,
+								layered_text.normalize(data, true), { depth: depthValue });
+						}
+						else if (cmdId === "sp-cmd-remove") {
+							if (elSel) layered_text_treeview.remove(elSel);
+						}
+						else if (cmdId === "sp-cmd-remove-children") {
+							if (elSel) layered_text_treeview.removeChildren(elSel);
+						}
+						else if (cmdId === "sp-cmd-remove-prop") {
+							if (elSelOne) layered_text_treeview.removeProperty(elSelOne);
+						}
+						else if (cmdId === "sp-cmd-update") {
+							if (elSelOne) layered_text_treeview.update(elSelOne, newName, newProp);
+						}
+
+						if (elNew && !layered_text_treeview.isSelectedMultiple(elNew)) {
+							layered_text_treeview.clickName(elNew);
+						}
+						else {
+							var elSelOne = layered_text_treeview.getOneSelected(container);
+							if (elSelOne) layered_text_treeview.clickName(elSelOne);
+						}
+					};
+
+				return "ui-test";
+			},
+			layered_text_treeview,
+			true
+		);
 	},
 
 };
